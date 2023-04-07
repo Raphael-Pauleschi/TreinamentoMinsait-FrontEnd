312aef0df79b38d900ae95b7bc1232854589c1ea
raphael-pauleschi <raphael.f.pauleschi@gmail.com>
2023-04-07 17:06:18 -0300

    Forms created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ListClientsComponent } from './pages/clients/list-clients/list-clients.component';
+import { RegisterEditClientsComponent } from './pages/clients/register-edit-clients/register-edit-clients.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'clients', component : ListClientsComponent }
+  {path: 'clients', component : ListClientsComponent },
+  {path: 'clients/register', component: RegisterEditClientsComponent},
+  {path: 'clients/edit/:cpf', component: RegisterEditClientsComponent}
 
 ];
 

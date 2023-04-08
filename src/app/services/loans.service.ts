367caabf64a2fbe3e006c166febfd0399fbd2d81
raphael-pauleschi <raphael.f.pauleschi@gmail.com>
2023-04-08 11:36:47 -0300

    Loan service completed and environment updated

--- src/app/services/loans.service.ts
+++ src/app/services/loans.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environment/environment';
+import { ILoan } from '../interfaces/loan';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoansService {
+  routeLink = `${environment.api}/clients`
+  endpoint = "loans"
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  findAllLoan(cpf: String){
+    return this.http.get<ILoan[]>(`${this.routeLink}/${cpf}/${this.endpoint}`);
+  }
+
+  findOneLoan(cpf:String, id:number){
+    return this.http.get<ILoan>(`${this.routeLink}/${cpf}/${this.endpoint}/${id}`)
+  }
+
+  registerLoan(cpf: String, loan: ILoan){
+    return this.http.post(`${this.routeLink}/${cpf}/${this.endpoint}`,loan);
+  }
+
+  deleteLoan(cpf: String, id: number){
+    return this.http.delete(`${this.routeLink}/${cpf}/${this.endpoint}/${id}`)
+  }
+
+
 }
+
